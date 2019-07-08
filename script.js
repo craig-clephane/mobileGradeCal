78ac49a094e7c1f8ec2db20653cc855ccbd47635
Craig <Craig-Clephane@hotmail.co.uk>
2019-07-08 21:47:49 +0100

    Added weights

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var label= "Assignment 1";
-var onStartUpLength = 2;
+var onStartUpLength = 0;
 var LengthofForms = 0;
 var total = 0;
 
 function addNewElement() {
+document.getElementById('gradeVisual').innerHTML = ""
 LengthofForms++;
-label = "Module " + LengthofForms;
-var node = '<div class = "gradeElement"><label for="input" class = "GradeHeader">'+label+'</label><input type="text" name="grade" maxlength="2" size="3"><br></div>';
+label = "Grade " + LengthofForms;
+label2 = "Weight " + LengthofForms;
+var node = '<div class = "gradeElement"><label for="input" class = "GradeHeader"></label><input type="text" id="grade'+LengthofForms+'" name="grade" maxlength="2" size="3"><br></div>';
+var percentagenode = '<div class = "weightElement"><label for="input" class = "GradeHeader"></label><input type="text"  id="weight'+LengthofForms+'" name="grade" maxlength="2" size="3"><br></div>';
 document.getElementById('formGradeElements').innerHTML += node;
+document.getElementById('formWeightElements').innerHTML += percentagenode;
 }
 
 
@@ -17,26 +21,65 @@ function onStartUp() {
 	}
 }
 
+
 function calculategrades() {
-	var forms = document.getElementById('formGradeElements');
+
 	var sum = 0;
 	var gradeelements = [];
+	var weightelements = [];
+	var addedelements = [];
+	var percentagecheck = 0;
+	var idcounter = 1;
 	for(i = 0; i < LengthofForms; i++) {
-		var formValue = forms.elements[i];
-		if (formValue.value == '') {
+		var grade = document.getElementById("grade"+idcounter+"").value;
+		var weight = document.getElementById("weight"+idcounter+"").value;
+		if (grade == '') {
 			alert("Please fill in empty values");
-			break;
+			return;
 		}
-		gradeelements.push(parseInt(formValue.value));
+		idcounter++;
+		gradeelements.push(parseInt(grade));
+		weightelements.push(parseInt(weight));
 	}
 	for(i = 0; i < LengthofForms; i++) {
-		sum=+sum+gradeelements[i];
+		percentagecheck=percentagecheck+weightelements[i];
 	}
-	total=(sum/LengthofForms);
-	printGrade(Math.round(total, 4));
+	if(percentagecheck != 100) {
+		alert("Percentage much equal 100");
+		return;
+	}
+	for(i = 0; i < LengthofForms; i++) {
+		addedelements.push(gradeelements[i]*weightelements[i]);		
+	}
+	for (i = 0; i < LengthofForms; i++) {
+		sum=sum+addedelements[i];
+	}
+	//total=(sum/LengthofForms);
+	sum=sum/100;
+	printGrade(sum);
 }
 
 function printGrade(grade) {
-	var gradeDisplay = '<div class = "gradeDisplay"><h1>'+grade+'%</h1></div>';
+	var gradetitle = ""
+	if (grade < 40) {
+		gradetitle = "Bitch you failed";
+	}
+	if (grade >= 40) {
+		gradetitle = "You passed";
+	}
+		if(grade >= 50) {
+		gradetitle = "You've acheieved a 2:2";
+	}
+		if(grade >= 60){
+		gradetitle = "You've acheieved a 2:1";
+	}
+
+	if(grade >= 70) {
+		gradetitle = "You've achieved a 1st!";
+	}
+
+
+	
+	var gradeDisplay = '<div class = "gradeDisplay"><h2>'+gradetitle+'</h2><h1>'+grade+'%</h1></div>';
 	document.getElementById('gradeVisual').innerHTML = gradeDisplay;
 }
