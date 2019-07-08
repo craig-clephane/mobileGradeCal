460ff81715997442dc7081bc2a1ddafb7df0b1c8
Craig <Craig-Clephane@hotmail.co.uk>
2019-07-08 01:24:16 +0100

    updated javascript

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var label= "Assignment 1";
-var onStartUpLength = 3;
+var onStartUpLength = 2;
 var LengthofForms = 0;
-
+var total = 0;
 
 function addNewElement() {
 LengthofForms++;
-label = "Assignment " + LengthofForms;
+label = "Module " + LengthofForms;
 var node = '<div class = "gradeElement"><label for="input" class = "GradeHeader">'+label+'</label><input type="text" name="grade" maxlength="2" size="3"><br></div>';
 document.getElementById('formGradeElements').innerHTML += node;
 }
@@ -32,5 +32,11 @@ function calculategrades() {
 	for(i = 0; i < LengthofForms; i++) {
 		sum=+sum+gradeelements[i];
 	}
-	console.log(sum);
+	total=(sum/LengthofForms);
+	printGrade(Math.round(total, 4));
+}
+
+function printGrade(grade) {
+	var gradeDisplay = '<div class = "gradeDisplay"><h1>'+grade+'%</h1></div>';
+	document.getElementById('gradeVisual').innerHTML = gradeDisplay;
 }
