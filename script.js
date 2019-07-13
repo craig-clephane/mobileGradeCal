ea31f3e283c4c95d06c031d900beae223b6cce78
Craig <Craig-Clephane@hotmail.co.uk>
2019-07-13 10:13:11 +0100

    updated ajavscript

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var label= "Assignment 1";
 var onStartUpLength = 0;
 var LengthofForms = 0;
 var total = 0;
+var ModuleNameGlobal = '';
 
 function addNewElement() {
 document.getElementById('gradeVisual').innerHTML = ""
@@ -27,12 +28,12 @@ function appendElement(grade, weight) {
 }
 }
 
-function onStartUp() {
+function onStartUp(ModuleName) {
 	var counter = 1;
-
-	if ("numberofItems" in localStorage) {
- 
-    var numberOfValues = localStorage.getItem('numberofItems');
+	ModuleNameGlobal = ModuleName;
+	if (ModuleName+"numberofItems" in localStorage) {
+ 	
+    var numberOfValues = localStorage.getItem(ModuleName+'numberofItems');
 	} else {
   
 	}
@@ -42,8 +43,8 @@ function onStartUp() {
 	}
 	LengthofForms = 0;
 	for(i = 0; i < numberOfValues; i++) {
-	var localgrade = localStorage.getItem("grade"+counter+"");
-	var localweight = localStorage.getItem("weight"+counter+"");
+	var localgrade = localStorage.getItem(ModuleName+"grade"+counter+"");
+	var localweight = localStorage.getItem(ModuleName+"weight"+counter+"");
 	appendElement(localgrade, localweight);
 	counter++;
 	}
@@ -109,18 +110,98 @@ function printGrade(grade) {
 }
 
 function saveGrade() {
+	var ModuleName = ModuleNameGlobal;
 	var idcounter = 1;
 	for(i = 0; i < LengthofForms; i++) {
 		var grade = document.getElementById("grade"+idcounter+"").value;
 		var weight = document.getElementById("weight"+idcounter+"").value;
-		localStorage.setItem("grade"+idcounter+"", grade);
-		localStorage.setItem("weight"+idcounter+"", weight);
+		localStorage.setItem(ModuleName+"grade"+idcounter+"", grade);
+		localStorage.setItem(ModuleName+"weight"+idcounter+"", weight);
 		idcounter++;
+		console.log("Saving "+ModuleName +"\n Grade "+grade+"\n Weight " +weight);
 	}
-	localStorage.setItem("numberofItems", LengthofForms);
+	localStorage.setItem(ModuleName+"numberofItems", LengthofForms);
 }
 
 function clearGrade() {
 	localStorage.clear();
 	location.reload(); 
+}
+
+
+
+var countOfModules = 0;
+function addNewModule() {
+	countOfModules++;
+	var modulename = document.getElementById("module_name").value;
+	console.log(modulename);
+	console.log(countOfModules);
+
+	var node = '<div class = "module" id="Module'+countOfModules+'" onClick=openModuleDetails("'+modulename+'")><div class="moduletitle"><p id = "ModuleP'+countOfModules+'">'+modulename+'</p></div><div class="modulePrecentage"><p>69%</p></div></div>';
+	document.getElementById('module_elements').innerHTML += node;
+	closepopup();
+
+}
+
+function appendModule(modulename) {
+	countOfModules++;
+	var node = '<div class = "module" id="Module'+countOfModules+'" onClick=openModuleDetails("'+modulename+'")><div class="moduletitle"><p id = "ModuleP'+countOfModules+'">'+modulename+'</p></div><div class="modulePrecentage"><p>69%</p></div></div>';
+	document.getElementById('module_elements').innerHTML += node;
+}
+
+
+function saveModules() {
+	var idcounter = 1;
+	for(i = 0; i < countOfModules; i++) {
+		var ModName = document.getElementById("ModuleP"+idcounter+"").innerHTML;
+		console.log(ModName);
+		localStorage.setItem("Module"+idcounter+"",ModName);
+		idcounter++;
+	}
+	localStorage.setItem("ModuleCount",countOfModules);
+}
+
+function openModuleDetails(modulename) {
+display_module();
+console.log("loading " +modulename);
+onStartUp(modulename);
+
+}
+function openpopup() {
+	document.getElementById('myModal').style.display = 'block';
+	document.getElementById("module_name").value = "";
+}
+function closepopup() {
+	document.getElementById('myModal').style.display = 'none';
+}
+
+
+function display_module() {
+	document.getElementById('below_container').style.display = 'block';
+	document.getElementById('module_options').style.display = 'block';
+	document.getElementById('overview_module').style.display = 'none';
+}
+
+function load() {
+	
+	if ('ModuleCount' in localStorage) {
+ 	
+    var numberOfModule = localStorage.getItem('ModuleCount');
+    countOfModules = numberOfModule;
+	} else {
+  	var numberOfModule = 0;
+	}
+	var counterr = 1;
+	for(i = 0; i < numberOfModule; i++) {
+	var Module_Name = localStorage.getItem("Module"+counterr);
+	counterr++;
+	appendModule(Module_Name);
+	}
+
+}
+
+function backToModuleOverview() {
+	document.getElementById('below_container').style.display = 'none';
+	document.getElementById('module_options').style.display = 'none';
+	document.getElementById('overview_module').style.display = 'block';
 }
